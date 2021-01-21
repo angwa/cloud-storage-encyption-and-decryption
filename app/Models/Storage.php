fe70877e25acbb9aaafa13e07f360bb1cfe9e230
angwa <angwamoses@gmail.com>
2021-01-21 06:05:39 -0800

    Model refactor

--- app/Models/Storage.php
+++ app/Models/Storage.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Storage extends Model
 {
-    //
+    protected $table="storages";
+    public function user()
+    {
+    	return $this->belongsTo(User::class);
+    }
 }
