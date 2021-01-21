b50d386f4016fb4613980561b22f29f75c4cb07b
angwa <angwamoses@gmail.com>
2021-01-21 08:10:02 -0800

    refactoring

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -5,20 +5,21 @@
 @section('contents')
 
   <div class="container">
-    <div class="col-12 login">
+    <div class="col-12 login" style="margin-top:-30px">
  <div class="row">
     <div class="col-md-4">
 
         <ul class="list-group">
-          <li class="list-group-item active">Dashboard</li>
-          <li class="list-group-item">Upload File</li>
+          <li class="list-group-item active">Dashboard</a></li>
+          <li class="list-group-item"><a href="jjjjj">Upload File</a></li>
           <li class="list-group-item">View File</li>
           <li class="list-group-item">Download</li>
           <li class="list-group-item">Update Profile</li>
         </ul>
     </div>
     <div class="col-md-8">
-        dhjdhjdj
+        <h4>{{Auth::user()->name}}</h4>
+        <hr>
     </div>
  </div>
     </div>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,6 +20,5 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/dashboard', 'HomeController@index')->name('home');
+Route::get('/dashboard', [HomeController::class, 'index'])->name('home');
 
-Route::get('/home', 'HomeController@index')->name('home');
